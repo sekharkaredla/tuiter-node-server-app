6c42731eb12ddfffdc884a9ed4eaad779b236705
Sekhar Karedla <sekhar.karedla@gmail.com>
2022-11-09 19:56:31 -0600

    a8 tuits apis

---
+++ controllers/tuits/tuits.js
@@ -0,0 +1,31 @@
+export default [
+    {
+        "_id": 1,
+        "topic": "Elon Musk",
+        "time": "2h",
+        "title": "React.js is a component based front end library that makes it very easy to build Single Page Applications or SPAs",
+        "tuit": "You want to wake up in the morning and think the future is going to be great - and that’s what being a spacefaring civilization is all about. It’s about believing in the future and thinking that the future will be better than the past. And I can’t think of anything more exciting than going out there and being among the stars",
+        "image": "/images/elon_musk.png",
+        "userName": "Iron Man",
+        "handle": "ironman",
+        "replies": 4231,
+        "retuits": 3500,
+        "likes": 665,
+        "liked": true
+    },
+    {
+        "_id": 2,
+        "topic": "",
+        "userName": "JavaScript",
+        "time": "1 day",
+        "title": "JavaScript is programming language that can run on browsers as well as desktops",
+        "tuit": "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.",
+        "image": "/images/ny_post.png",
+        "tweets": "123K",
+        "handle": "jstuit",
+        "replies": 965,
+        "retuits": 2407,
+        "likes": 4962,
+        "liked": false
+    }
+];
